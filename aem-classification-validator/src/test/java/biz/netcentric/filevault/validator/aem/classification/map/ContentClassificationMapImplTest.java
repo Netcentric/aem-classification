8325a4ae67fd6eed289712a8826ff57ca92b5bcf
Konrad Windszus <konrad.windszus@netcentric.biz>
2022-07-28 08:56:30 +0200

    fix license headers

--- aem-classification-validator/src/test/java/biz/netcentric/filevault/validator/aem/classification/map/ContentClassificationMapImplTest.java
+++ aem-classification-validator/src/test/java/biz/netcentric/filevault/validator/aem/classification/map/ContentClassificationMapImplTest.java
@@ -4,10 +4,10 @@ package biz.netcentric.filevault.validator.aem.classification.map;
  * #%L
  * AEM Classification Validator
  * %%
- * Copyright (C) 2020 Netcentric - A Cognizant Digital Business
+ * Copyright (C) 2022 Cognizant Netcentric
  * %%
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * #L%
